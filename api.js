c6c6fc6541f032f716cf87aaaefff4fd45c76115
genamontana <predator199@yandex.ru>
2023-04-26 16:18:15 +0300

    Completed

--- api.js
+++ api.js
@@ -1,12 +1,14 @@
 const api = {
     sendStudentsCountToItKamasutra(studentsCount) {
-        return axios.post("https://it-kamasutra.com/students-need", 
-        { count: studentsCount });
+        return axios.post("https://it-kamasutra.com/students-need",
+            {count: studentsCount});
     },
     getVacanciesCountFromMicrosoft() {
-        return axios.get("https://microsoft.com");
+        return axios.get("https://microsoft.com")
+            .then((res) => res.data.vacancies)
     },
     getVacanciesCountFromGoogle() {
-        return axios.get("https://google.com");
+        return axios.get("https://google.com")
+            .then((res) => res.data.vacancies)
     }
 }
